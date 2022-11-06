53f757370b4460f01123f9f5f4900959b7ca6b5e
Haski21 <81989841+Haski21@users.noreply.github.com>
2022-11-06 14:42:22 +0300

    Update m4_t2_hw.py

--- m4_t2_hw.py
+++ m4_t2_hw.py
@@ -9,22 +9,23 @@ for _ in range(10):
 не используя min(), sort()
 """  
 from random import randint
+# nums = [10, 2, 1, 5, 2,4]
 nums = []
-
-for _ in range(10):
-    nums.append(randint(1, 100))
-print(nums)
+# for _ in range(10):
+#     nums.append(randint(1, 100))
+print(*nums)
     
 var_one = var_two = 101
 index = 0
 for _ in range(len(nums)):
-    if var_one >= nums[_]:
+    if var_one > nums[_]:
         var_one = nums[_]
         index = _
 nums.pop(index)
 for _ in range(len(nums)):
-    if var_two >= nums[_]:
+    if var_two > nums[_]:
         var_two = nums[_]
         index = _
-
+# nums.pop(index)
 print(f'Two min number are: {var_one} and {var_two}')
+# print(*nums)
